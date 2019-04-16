ef9607884b51ae957ec7498d4ebcab885d403e09
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2019-04-16 12:35:08 +0200

    sync url with recipe list filter

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { RecipeDataService } from '../recipe-data.service';
 import { Observable, Subject } from 'rxjs';
 import { Recipe } from '../recipe.model';
@@ -10,24 +11,36 @@ import { distinctUntilChanged, debounceTime, map } from 'rxjs/operators';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  public filterRecipeName: string;
+  public filterRecipeName: string = '';
   public filterRecipe$ = new Subject<string>();
 
   private _fetchRecipes$: Observable<Recipe[]> = this._recipeDataService
     .recipes$;
   public loadingError$ = this._recipeDataService.loadingError$;
 
-  constructor(private _recipeDataService: RecipeDataService) {
+  constructor(
+    private _recipeDataService: RecipeDataService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
     this.filterRecipe$
       .pipe(
         distinctUntilChanged(),
-        debounceTime(400),
-        map(val => val.toLowerCase())
+        debounceTime(400)
       )
-      .subscribe(val => (this.filterRecipeName = val));
-  }
+      .subscribe(val => {
+        const params = val ? { queryParams: { filter: val } } : undefined;
+        this._router.navigate(['/recipe/list'], params);
+      });
 
-  ngOnInit() {}
+    this._route.queryParams.subscribe(params => {
+      if (params['filter']) {
+        this.filterRecipeName = params['filter'];
+      }
+    });
+  }
 
   get recipes$() {
     return this._fetchRecipes$;
